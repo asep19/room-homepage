c69fa5fd3df5b71abf9adf1dd2365c20a148987b
asep19 <aseprendi487@gmail.com>
2022-06-10 08:40:30 +0700

    setup website theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    colors: { 
+      black: "hsl(0, 0%, 0%)",
+      white: "hsl(0, 0%, 100%)"
+    },
     extend: {
       colors: {
-        
+        darkgray: "hsl(0, 0%, 63%)",
+        verydarkgray: "hsl(0, 0%, 27%)"
       }
     },
   },
