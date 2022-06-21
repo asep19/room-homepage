e600e8b993d93ded57d39bfbb4ff2faac2f9e3fc
asep19 <aseprendi487@gmail.com>
2022-06-21 09:37:45 +0700

    delete unused text

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
-  theme: {
-    colors: { 
-      black: "hsl(0, 0%, 0%)",
-      white: "hsl(0, 0%, 100%)"
-    },
+  theme: { 
     extend: {
       colors: {
         darkgray: "hsl(0, 0%, 63%)",
-        verydarkgray: "hsl(0, 0%, 27%)"
+        verydarkgray: "hsl(0, 0%, 27%)",
+        black: "hsl(0, 0%, 0%)",
+        white: "hsl(0, 0%, 100%)"
+      },
+      screens: {
+        'sm': '440px'
       }
     },
   },
